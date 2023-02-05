e934b5633895f29ff0de5863a00d2fc06bfc1e88
MaxChuloshnikov <Maks447@ukr.net>
2023-02-05 12:17:41 +0200

    import Settings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from 'react-icons/f1';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import "./App.css";
 
 
 const App = () => {
     return (
-        <div className="underline text-3xl">App</div>
+        <h1 className="underline text-3xl">App</h1>
     )
 }
 
