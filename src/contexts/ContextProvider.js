d4ea9ef36b227472b6fb5f44decb2e9f6669016f
MaxChuloshnikov <Maks447@ukr.net>
2023-02-06 18:15:46 +0200

    useContent Done
    need to bug fixes

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -3,3 +3,31 @@ import React, {
     useContext,
     useState
 } from "react";
+
+
+const StateContext = createContext();
+
+const initialState = {
+    chat: false,
+    cart: false,
+    userProfile: false,
+    notification: false,
+}
+
+export const ContextProvider = ({ children }) => {
+
+    const [activeMenu, setActiveMenu] = useState(true);
+
+    return (
+        <StateContext.Provider
+        value={{
+            activeMenu,
+            setActiveMenu,
+        }}
+        >
+            { children }
+        </StateContext.Provider>
+    ) 
+}
+
+export const useStateContext = () => useContext(StateContext);
