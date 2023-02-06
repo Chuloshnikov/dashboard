036f64a14075cffee7b71881ddc4441047726755
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-02-06 11:55:14 +0200

    Sidebar-start

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Link, Navlink } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+import { TooltipComponent} from "@syncfusion/ej2-react-popups";
+
+import { links } from "../data/dummy";
 
 const Sidebar = () => {
+    const activeMenu = true;
+
+
     return (
-        <div>
-            Sidebar
+        <div className="ml-3 h-screen 
+        md:overflow-hiden 
+        overflow-auto 
+        pb-10">
+            { activeMenu && (<>
+            <div className="flex justify-between items-center">
+                <Link to='/' onClick={() => {}}
+                className="items-center">
+                    <SiShopware /> <span>Shoppy</span>
+                </Link>
+            </div>
+            
+            </>)}
         </div>
     )
 }
